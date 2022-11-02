c3305040194d3a87d8d07137c8a85ed6cf2e9ed6
jeanls-solano <jeanlucaseh10@gmail.com>
2022-11-02 19:38:58 -0400

    start front-end for web

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,37 @@
 import Fastify from "fastify";
+import cors from '@fastify/cors'
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient({
+    log: ['query'],
+})
 
 async function bootstrap() {
     const fastify = Fastify({
         logger: true,
     })
 
-    fastify.get("/pools/count", () =>{
-        return { count: "12/34" }
+    await fastify.register(cors, {
+        origin: true,
+    })
+    //config temp
+
+    fastify.get("/pools/count", async () =>{
+        const pools = await prisma.pool.findMany({
+            where: {
+                code:{
+                    startsWith: "J"
+                }
+            }
+        })
+
+        const count = await prisma.pool.count()
+
+
+        return { count }
     })
 
-    await fastify.listen({port: 3333})
+    await fastify.listen({port: 3333, host: '0.0.0.0' })
 
 }
 
